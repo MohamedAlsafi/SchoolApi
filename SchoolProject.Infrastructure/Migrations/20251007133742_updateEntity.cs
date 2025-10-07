a95d1da6018adaf652fbb70fcd33931ef7e7fc6e
MohamedAlsafi <mohamed.m.alsafi@gmail.com>
2025-10-07 16:44:27 +0300

    mod

---
+++ SchoolProject.Infrastructure/Migrations/20251007133742_updateEntity.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchoolProject.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "Subjects",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "StudentSubjects",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "Students",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "DepartmentSubjects",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "Departments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "Subjects");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "StudentSubjects");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "DepartmentSubjects");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "Departments");
+        }
+    }
+}

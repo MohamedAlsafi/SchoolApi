c6921576c7f3b3f43365119a5144d33b8930dea9
MohamedAlsafi <mohamed.m.alsafi@gmail.com>
2025-11-13 14:31:47 +0200

    Add GlobalTransactionMiddleware

---
+++ SchoolProject.Api/Middlewares/GlobalTransactionMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using SchoolProject.Infrastructure.Data;
+
+namespace SchoolProject.Api.Middlewares
+{
+    public class GlobalTransactionMiddleware : IMiddleware
+    {
+        private readonly SchoolDbContext _dbContext;
+        public GlobalTransactionMiddleware(SchoolDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            IDbContextTransaction Transaction = null!;
+            try
+            {
+                Transaction = _dbContext.Database.BeginTransaction();
+                await next(context);
+                Transaction.Commit();
+
+            }
+            catch (Exception)
+            {
+
+                Transaction?.Rollback();
+                throw;
+            }
+
+
+        }
+    }
+}
